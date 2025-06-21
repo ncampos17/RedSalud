905e6c5060b9493134d4cbdb638b08f7211376c9
ncampos17 <nico.campost@gmail.com>
2025-06-21 00:40:19 -0400

    Página de Recuperar Contraseña
    Se hizo la creación de la pagina para Recuperar Contraseña, en donde sigue el mismo diseño de la pagina LOGIN.
    Para entrar a ella se debe ingresar "/recuperar" en el link de localhost.

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { LoginComponent } from './components/login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: LoginComponent },
+  { path: 'recuperar', component: ForgotPasswordComponent }
+];
+
