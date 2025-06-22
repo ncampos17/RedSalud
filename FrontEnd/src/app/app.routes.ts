01543a3ad7b2407e805ad659f6f2839d5420c66b
ncampos17 <nico.campost@gmail.com>
2025-06-21 22:22:52 -0400

    Página REGISTER
    Se creo la pagina de registro, en donde uno se puede crear una cuenta de RedSalud. No sirve , momentáneamente, los botones de "Registrarse" y "Volver al inicio"

--- FrontEnd/src/app/app.routes.ts
+++ FrontEnd/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'recuperar', component: ForgotPasswordComponent }
+  { path: 'recuperar', component: ForgotPasswordComponent },
+  { path: 'registro', component: RegisterComponent }
+
 ];
 
