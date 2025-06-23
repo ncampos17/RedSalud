6d33dee4ff81987f34388ed13b23336d470ee584
Ricardo <chirinos.r2908@gmail.com>
2025-06-23 14:02:14 -0400

    feat: enlace de login al dashboard
    Se hizo la coneccion desde el inicio de sesion hasta el dashboard

--- FrontEnd/src/app/components/login/login.component.ts
+++ FrontEnd/src/app/components/login/login.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  email = '';
+  password = '';
+  errorMsg = '';
 
+  constructor(private authService: AuthService, private router: Router) {}
+
+  onLogin() {
+    this.authService.login(this.email, this.password).subscribe(result => {
+      if (result.success) {
+        if (result.role === 'admin') {
+          this.router.navigate(['/dashboard']); // acceso total
+        } else if (result.role === 'especialista') {
+          this.router.navigate(['/dashboard']); // puedes cambiar la ruta si hay una vista específica
+        }
+      } else {
+        this.errorMsg = 'Credenciales incorrectas';
+      }
+    });
+  }
 }
