0afcdd3bc7f95e8098bb456af189ebb1a664f1d7
Ricardo <chirinos.r2908@gmail.com>
2025-06-23 23:28:09 -0400

    feat: visualizar reportes

--- FrontEnd/src/app/components/reporte/reporte.component.ts
+++ FrontEnd/src/app/components/reporte/reporte.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-reporte',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './reporte.component.html',
   styleUrl: './reporte.component.css'
 })
-export class ReporteComponent {
+export class ReporteComponent {
 
 }
