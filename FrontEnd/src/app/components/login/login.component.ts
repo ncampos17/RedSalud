82fbc6ef9b3911923c35b5548162a0b97fd2c479
Ricardo <chirinos.r2908@gmail.com>
2025-06-23 20:53:27 -0400

    feature: enlae entre pestañas

--- FrontEnd/src/app/components/login/login.component.ts
+++ FrontEnd/src/app/components/login/login.component.ts
@@ -3,32 +3,15 @@ import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
-  email = '';
-  password = '';
-  errorMsg = '';
+export class LoginComponent {
 
-  constructor(private authService: AuthService, private router: Router) {}
-
-  onLogin() {
-    this.authService.login(this.email, this.password).subscribe(result => {
-      if (result.success) {
-        if (result.role === 'admin') {
-          this.router.navigate(['/dashboard']); // acceso total
-        } else if (result.role === 'especialista') {
-          this.router.navigate(['/dashboard']); // puedes cambiar la ruta si hay una vista específica
-        }
-      } else {
-        this.errorMsg = 'Credenciales incorrectas';
-      }
-    });
-  }
 }
